ffe2bb9b831c4c95b76c09b5092d698cb96b5358
summerlanchan <111342458+summerlanchan@users.noreply.github.com>
2022-12-04 23:51:09 +0900

    Fixed the link

--- src/FooterLink.js
+++ src/FooterLink.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function FooterLink() {
-  return <div className="FooterLink">Coded by Victoria</div>;
+  return (
+    <div className="FooterLink">
+      Coded by <a href="https://github.com/summerlanchan">Victoria</a>
+    </div>
+  );
 }
